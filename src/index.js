a52923858b0fc4250fe8f9b31b35ac793ef13393
Yapo Annoux Assi <yapanss@yahoo.fr>
2018-06-07 18:27:54 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import RecipeCo from './RecipeCo'
-import '../appli.css'
+import './appli.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
